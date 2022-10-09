0c43a62761381d186ce29327f2393576b3c0ba44
Daria Kurmaeva <lakchmu@gmail.com>
2022-10-09 19:58:55 +0400

    feat: MC-4 setup new Frontend app on nuxt3

---
+++ Frontend/pages/hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+let text = ref<any>('Init text');
+
+const { data } = await useFetch('/api/test');
+
+if(data) {
+  text = data
+}
+</script>
