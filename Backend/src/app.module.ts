214702fb4e6c2e60c80871844956d3482cb00eef
Daria Kurmaeva <lakchmu@gmail.com>
2022-11-11 10:20:36 +0400

    fix: MC-7 fix AppModule 2

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,17 +1,14 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from './prisma.service';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CollectionController } from './collection/collection.controller';
-import { CollectionService } from './collection/collection.service';
 import { CollectionModule } from './collection/collection.module';
 
 @Module({
   imports: [AuthModule, UsersModule, CollectionModule],
-  controllers: [AppController, CollectionController],
-  providers: [AppService, CollectionService, PrismaService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
